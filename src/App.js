977bab21e6950f7703e1f2090eaf98549a55728b
ckott10 <ckott10@gmail.com>
2024-04-30 03:18:07 +0300

    Изменил логику счетчика

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
+        <h2>Счетчик!:</h2>
         <h1>{count}</h1>
         <button onClick={onClickMinus} className="minus">- Минус</button>
         <button onClick={onClickPlus} className="plus">Плюс +</button>
